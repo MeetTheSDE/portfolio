e7eaf07a3a2ac4f2c3b042b6ef5882fc0df05554
MeetTheSDE <patelmeet8331@gmail.com>
2025-02-22 12:51:12 -0800

    container.js changes

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,7 @@ const Container = styled.div`
   width: 80%;
   max-width: 1200px;
   margin: 0 auto;
-  padding: 50px 0; /* Adds spacing from top */
+  padding: 50px 0;
 `;
 
 export default Container;
