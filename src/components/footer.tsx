83a93312b8414a44013671479720f1cc1065ca30
MeetTheSDE <patelmeet8331@gmail.com>
2025-08-16 16:07:47 -0700

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,15 @@
 import { Github, Linkedin, Mail } from "lucide-react";
+import { PiXLogo } from "react-icons/pi";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 text-white py-12 dark:bg-gray-900 dark:text-muted-foreground dark:border-t dark:border-border">
+        <footer className="bg-gray-900 text-white py-8 dark:bg-gray-900 dark:text-muted-foreground dark:border-t dark:border-border">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="text-center">
-                    <p className="text-gray-400 mb-4 dark:text-muted-foreground/80">
+                <div className="flex flex-col sm:flex-row justify-between items-center">
+                    <p className="text-gray-400 mb-4 sm:mb-0 dark:text-muted-foreground/80">
                         © 2025 Meetkumar Patel. All rights reserved.
                     </p>
-                    <div className="flex justify-center space-x-6">
+                    <div className="flex space-x-6">
                         <a
                             href="https://www.linkedin.com/in/meet1809/"
                             target="_blank"
@@ -33,6 +34,15 @@ export default function Footer() {
                             aria-label="Email"
                         >
                             <Mail className="h-6 w-6" />
+                        </a>
+                        <a
+                            href="https://x.com/realMeetPatel"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-400 hover:text-white transition-colors dark:text-muted-foreground dark:hover:text-primary"
+                            aria-label="X"
+                        >
+                            <PiXLogo className="h-6 w-6" />
                         </a>
                     </div>
                 </div>
